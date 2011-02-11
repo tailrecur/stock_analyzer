054f82200f7530bacae63a1e3a0da49ba4e7983b
dlewis <deepak.lewis@gmail.com>
2011-02-11 20:41:46 +0530

    Initialized Rails 3 project, copied gyan and TODO from older projects

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockAnalyzer::Application.config.secret_token = '0155540bf5c1f811bd9aa8f121a724789075773f5b4d60d6894d90dd1abc7a2f094f4bbc3e6e32329366c81ba36f03d73eb74557492ce5eae21a2ace2e150116'
