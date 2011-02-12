1f3a8042e9a1518934aed16f2180fb28cbc7fd53
dlewis <deepak.lewis@gmail.com>
2011-02-12 17:26:31 +0530

    Added balance_sheet columns

--- db/migrate/20110212111216_create_balance_sheets.rb
+++ db/migrate/20110212111216_create_balance_sheets.rb
@@ -3,8 +3,25 @@ class CreateBalanceSheets < ActiveRecord::Migration
     create_table :balance_sheets do |t|
       t.date :period_ended
 
+      t.float :total_share_capital, :equity_share_capital, :share_application_money, :preference_share_capital
+      t.float :reserves, :revaluation_reserves, :net_worth, :secured_loans, :unsecured_loans
+      t.float :total_debt, :total_liabilities
+      t.float :gross_block, :accumulated_depreciation, :net_block, :capital_work_in_progress
+      t.float :investments, :inventories, :sundry_debtors, :cash_and_bank_balance
+      t.float :total_current_assets, :loans_and_advances, :fixed_deposits, :deferred_credit
+      t.float :current_liabilities, :provisions, :net_current_assets, :misc_expenses
+      t.float :total_assets, :contingent_liabilities, :book_value
+
+      #Required by banks
+      t.float :deposits, :borrowings, :other_liabilities
+      t.float :balance_with_rbi, :money_at_call, :advances
+      t.float :other_assets, :bills_for_collection
+
+      t.string :type
+      t.references :company
       t.timestamps
     end
+    add_index :balance_sheets, [:company_id, :period_ended], :unique => true
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,55 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110211180511) do
+ActiveRecord::Schema.define(:version => 20110212111216) do
+
+  create_table "balance_sheets", :force => true do |t|
+    t.date     "period_ended"
+    t.float    "total_share_capital"
+    t.float    "equity_share_capital"
+    t.float    "share_application_money"
+    t.float    "preference_share_capital"
+    t.float    "reserves"
+    t.float    "revaluation_reserves"
+    t.float    "net_worth"
+    t.float    "secured_loans"
+    t.float    "unsecured_loans"
+    t.float    "total_debt"
+    t.float    "total_liabilities"
+    t.float    "gross_block"
+    t.float    "accumulated_depreciation"
+    t.float    "net_block"
+    t.float    "capital_work_in_progress"
+    t.float    "investments"
+    t.float    "inventories"
+    t.float    "sundry_debtors"
+    t.float    "cash_and_bank_balance"
+    t.float    "total_current_assets"
+    t.float    "loans_and_advances"
+    t.float    "fixed_deposits"
+    t.float    "deferred_credit"
+    t.float    "current_liabilities"
+    t.float    "provisions"
+    t.float    "net_current_assets"
+    t.float    "misc_expenses"
+    t.float    "total_assets"
+    t.float    "contingent_liabilities"
+    t.float    "book_value"
+    t.float    "deposits"
+    t.float    "borrowings"
+    t.float    "other_liabilities"
+    t.float    "balance_with_rbi"
+    t.float    "money_at_call"
+    t.float    "advances"
+    t.float    "other_assets"
+    t.float    "bills_for_collection"
+    t.string   "type"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "balance_sheets", ["company_id", "period_ended"], :name => "index_balance_sheets_on_company_id_and_period_ended", :unique => true
 
   create_table "companies", :force => true do |t|
     t.string   "name"
