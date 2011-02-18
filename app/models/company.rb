a2ea78aa9fe5a74009e266b55bf3a5cce34cf65d
dlewis <deepak.lewis@gmail.com>
2011-02-18 15:51:22 +0530

    Added specs for eps

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,9 @@ class Company < ActiveRecord::Base
   has_many :balance_sheets
   has_many :quarterly_results
   has_many :profit_and_losses
+
+  def eps
+    quarters = quarterly_results[0..3]
+    quarters.size < 4 ? -1 : quarters.inject(0) { |sum, quarterly_result| sum + quarterly_result.eps }
+  end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
+RSpec::Matchers.define :have_eps do |expected|
+  chain(:for) { |number| @number = number }
+  chain(:quarters) { @number.times { company.quarterly_results << Factory.build(:quarterly_result, :eps => 5) } }
+
+  match do |actual|
+    actual.eps == expected
+  end
+
+  description do
+    "have be #{expected} for #{@number} quarters with eps 5"
+  end
+end
+
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "eps" do
+    subject { company }
+    let(:company) { Factory.build(:company) }
+
+    it { should have_eps(20).for(4).quarters }
+    it { should have_eps(20).for(6).quarters }
+    it { should have_eps(-1).for(3).quarters }
+  end
 end
