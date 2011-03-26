35db91a5ea00f9c32afc1cb35d9927717a6886aa
dlewis <deepak.lewis@gmail.com>
2011-03-26 19:10:55 +0530

    Used jquery ui tabs and changed ui theme to redmond

--- public/javascripts/companies/show.js
+++ public/javascripts/companies/show.js
@@ -1,32 +1,10 @@
-$(document).ready(function() {
-  var viewModel = {
-    retrieve: function(url) {
-      $.getJSON(url, { per_page: 5, format: 'json' }, function(data) {
-        ko.applyBindings(new Parser(data));
-      });
-
-    },
-    periods: [],
-    rows: []
-  };
-
-  ko.applyBindings(viewModel);
+$("#tabs").tabs({
+    show: function(event, ui) {
+        var url = $(ui.tab).attr('data-url');
+        if(url) {
+            $.getJSON(url, { per_page: 5, format: 'json' }, function(data) {
+                ko.applyBindings(new Parser(data));
+            });
+        }
+    }
 });
-
-//$("#tabs").tabs({
-//    select: function(event, ui) {
-//        url = $.data(ui.tab, 'load.tabs');
-//        $(ui.panel).html("testttttttassssssssssinnnnnnnn111111111111nnnnnn");
-//        alert(1);
-//        $.ajax({
-//            url: url,
-//            success: function(data) {
-//                alert($(ui.panel).html());
-////                parser = new Parser(data);
-//                alert(ui.panel);
-//                $(ui.panel).html("testttttttassssssssssinnnnnnnnnnnnnn");
-//            }
-//        });
-//        return true;
-//    }
-//});
